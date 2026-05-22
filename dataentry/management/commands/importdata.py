c50720be923508b650e8c12bccb66dced37f27f5
Harisenthil-hub <hari367854@gmail.com>
2026-05-22 14:15:26 +0530

    Feat: imported data from csv to db

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,24 +1,35 @@
-from django.core.management.base import BaseCommand
-from dataentry.models import Student
+from django.core.management.base import BaseCommand, CommandError
+# from dataentry.models import Student
+from django.apps import apps
 import csv
 
-
+# Proposed Command - python manage.py importdata file_path model_name
 class Command(BaseCommand):
     help = 'Import data from csv file'
     
     def add_arguments(self, parser):
         parser.add_argument('file_path', type=str, help='Path to the csv file')
+        parser.add_argument('model_name', type=str, help='Name of the model')
         
     def handle(self, *args, **kwargs):
         file_path = kwargs['file_path']
+        model_name = kwargs['model_name'].capitalize()
+        
+        
+        model = None
+        for app_config in apps.get_app_configs():
+            
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break
+            except LookupError:
+                continue
+            
+        if not model:
+            raise CommandError(f'Modal "{model_name}" not found')
         with open(file_path, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if not Student.objects.filter(roll_no=row['roll_no']).exists():
-                   Student.objects.create(**row)
-                else:
-                    self.stdout.write(
-                        self.style.WARNING(f'Student with this roll no {row['roll_no']} already exists.')
-                    )
+                model.objects.create(**row)
         
         self.stdout.write(self.style.SUCCESS('Data imported from CSV Successfully!'))
